79d680e18390e860407cdbf92d6a952fe0832e8a
noelachkar <noelachkar@hotmail.com>
2020-09-28 13:24:52 +0300

    Initial Commit

---
+++ CitySearch/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CitySearch
+//
+//  Created by Noel Achkar on 9/28/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
